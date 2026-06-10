fd53980b25a7b056e3e8539f75aa28eb4f591cb8
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-10 13:25:40 +0545

    Prepare home page for  project default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,19 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-background text-foreground">
+        <header className="border-b border-background bg-gray-800 backdrop-blur-2xl">
+          <div className="mx-auto flex h-8 w-full max-w-6xl items-center justify-between px-4">
+            <Link href={"/"} className="text-bold tracking-wider text-sm">Event Planner</Link>
+            <nav className="flex items-center gap-4">
+              <Link href={"/dashboard"}>Dashboard</Link>
+            </nav>
+          </div>
+        </header>
+        <main className="mx-auto flex flex-col w-full grow px-4 py-4 max-w-6xl">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
