fb1a150e5973b0b4b620b9e868903dd40076683a
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-10 16:11:33 +0545

    Fix middleware gatekeeping CSS and other static files

--- proxy.ts
+++ proxy.ts
@@ -5,3 +5,13 @@ export default async function proxy(request: NextRequest) {
         return auth.middleware({ loginUrl: "/auth/sign-in" })(request);
         // This function checks if the user is authenticated on EVERY request inside the app and redirect to the loginUrl if not.
 }
+
+export const config = {
+        matcher: [
+                "/((?!_next/static|_next/image|favicon.ico).*)"
+        ],
+        /* Don't ask me this because this was in Next.js docs:
+                1. https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
+                2. https://github.com/pillarjs/path-to-regexp#path-to-regexp-1
+        */
+}
