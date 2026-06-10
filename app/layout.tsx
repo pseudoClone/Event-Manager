ea61d28d24b14236180b5e6ab6b618d6d56b166f
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-10 14:42:47 +0545

    Add suppression warning prop as asked by Neon Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
+      suppressHydrationWarning
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
