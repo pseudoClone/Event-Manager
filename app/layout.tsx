6f849c389327d16769f228ab5d00b928a8100d96
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-10 14:49:36 +0545

    1. Wrap content in Neon Auth UI Provider Component 2. Fix nav bar height for User Button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { NeonAuthUIProvider, UserButton } from "@neondatabase/auth/react";
+import { authClient } from "@/lib/auth/client";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,17 +26,22 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <header className="border-b border-background bg-gray-800 backdrop-blur-2xl">
-          <div className="mx-auto flex h-8 w-full max-w-6xl items-center justify-between px-4">
-            <Link href={"/"} className="text-bold tracking-wider text-sm">Event Planner</Link>
-            <nav className="flex items-center gap-4">
-              <Link href={"/dashboard"}>Dashboard</Link>
-            </nav>
-          </div>
-        </header>
-        <main className="mx-auto flex flex-col w-full grow px-4 py-4 max-w-6xl">
-          {children}
-        </main>
+        <NeonAuthUIProvider authClient={authClient}>
+          <header className="border-b border-background bg-gray-800 backdrop-blur-2xl">
+            <div className="mx-auto flex w-full max-w-6xl items-center justify-between px-4 h-16">
+              <Link href={"/"} className="text-bold tracking-wider text-sm">Event Planner</Link>
+              <nav className="flex items-center gap-4">
+                <Link href={"/dashboard"}>
+                  Dashboard
+                </Link>
+                <UserButton size="icon" />
+              </nav>
+            </div>
+          </header>
+          <main className="mx-auto flex flex-col w-full grow px-4 py-4 max-w-6xl">
+            {children}
+          </main>
+        </NeonAuthUIProvider>
       </body>
     </html>
   );
