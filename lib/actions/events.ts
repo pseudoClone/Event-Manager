6ee9962101b5d5c917dcc354dee8d63e02c6f570
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-10 22:52:52 +0545

    Create server action to handle user form input

---
+++ lib/actions/events.ts
@@ -0,0 +1,42 @@
+"use server";
+import { redirect } from "next/navigation";
+import { getSession } from "../auth/server";
+import { prisma } from "../prisma";
+
+function parseCreateEvent(formData: FormData) {
+        const title = String(formData.get("title") ?? "").trim();
+        if (title.length < 3) {
+                throw new Error("Title Length must be more than 3")
+        }
+
+        const description = String(formData.get("description") ?? "").trim();
+        const location = String(formData.get("location") ?? "").trim();
+        const eventDate = String(formData.get("eventDate") ?? "").trim();
+
+        return {
+                title,
+                description: description.length ? description.slice(0, 2000) : null,
+                location: location.length ? location.slice(0, 200) : null,
+                eventDate: eventDate.length ? eventDate : null,
+        }
+}
+
+export default async function createEventAction(formData: FormData) {
+        const session = await getSession();
+        const userId = session.data?.user.id;
+        if (!userId) {
+                throw new Error("Unauthorized");
+        }
+        const input = parseCreateEvent(formData);
+
+        const parsedInput = await prisma.event.create({
+                data: {
+                        ownerUserId: userId,
+                        title: input.title,
+                        description: input.description,
+                        eventDate: input.eventDate ? new Date(input.eventDate) : null,
+                },
+        });
+        redirect(`/events/${parsedInput.id}`);
+}
+
