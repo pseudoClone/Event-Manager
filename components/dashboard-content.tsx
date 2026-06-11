9f9ee03783814af13823d0ae2673fd53dac68558
pseudoclone <prince.077bei027@acem.edu.np>
2026-06-11 10:03:26 +0545

    Show created events in dashboard

--- components/dashboard-content.tsx
+++ components/dashboard-content.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { prisma } from "@/lib/prisma";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Badge } from "./ui/badge";
 
 export default async function DashboardContent({ userId }: { userId: string }) {
 
@@ -34,6 +37,55 @@ export default async function DashboardContent({ userId }: { userId: string }) {
                                         <Link href={"/events/new"}>Create Event</Link>
                                 </Button>
                         </div>
+
+                        {events.length === 0
+                                ?
+                                <Card>
+                                        <CardHeader>
+                                                <CardTitle> No events </CardTitle>
+                                        </CardHeader>
+                                        <CardContent>
+                                                <p className="text-sm text-muted-foreground">
+                                                        Create your first event
+                                                </p>
+                                        </CardContent>
+                                </Card>
+                                :
+                                <div className="grid gap-2 md:grid-cols-2">
+                                        {
+                                                events.map((event) => (
+                                                        <Card key={event.id}>
+                                                                <CardHeader className="space-y-2">
+                                                                        <div className="flex items-start justify-between">
+                                                                                <CardTitle>
+                                                                                        {event.title}
+                                                                                </CardTitle>
+                                                                                <Button asChild size={"xs"}>
+                                                                                        <Link href={`/events/${event.id}`}>Open</Link>
+                                                                                </Button>
+                                                                        </div>
+                                                                        <div>
+                                                                                <Badge variant={"secondary"} />
+                                                                                <Badge variant={"secondary"} />
+                                                                                <Badge variant={"secondary"} />
+                                                                        </div>
+                                                                        <p>
+                                                                                {event.eventDate ? new Date(event.eventDate).toLocaleString()
+                                                                                        :
+                                                                                        "No date mentioned"
+                                                                                }
+                                                                                <div>
+                                                                                        {event.location ? `${event.location}`
+                                                                                                : "No location mentioned"}
+                                                                                </div>
+                                                                        </p>
+                                                                </CardHeader>
+                                                        </Card>
+                                                ))
+                                        }
+                                </div>
+
+                        }
                 </div>
         );
 }
